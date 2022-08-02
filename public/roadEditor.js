8f8645ee8fa08c40f662ca954520efb1ce129d56
TTT <0716thomas@gmail.com>
2022-08-02 12:20:29 +0800

    [Initialize] initial commit

---
+++ public/roadEditor.js
@@ -0,0 +1,369 @@
+var landElement = document.getElementById("land");
+var editorElement = document.getElementById("editor");
+
+var roadTemplate = document.getElementById("land");
+var sidewalkTemplate = document.getElementById("land");
+var bollardTemplate = document.getElementById("land");
+var roadComponentTemplate = document.getElementById("land");
+
+var templateBase = {};
+
+var landWidth = 15;
+var dragElement = null;
+var hitboxCounter = 0;
+var componentCounter = 0;
+var inHitboxId = null;
+var touchHitbox = false;
+var draging = false;
+
+const componentDefaultWidth = {
+    "road": 3,
+    "sidewalk": 1,
+    "bollard": 0.5,
+}
+
+function LandInit() {
+    const hitboxTemplate = document.getElementById("hitboxTemplate");
+    landElement.innerHTML = "";
+    AddHitbox();
+}
+
+function OnLoad() {
+    console.log("load");
+    //set element
+    landElement = document.getElementById("land");
+    editorElement = document.getElementById("editor");
+
+    //get template element from document
+    templateBase["road"] = document.getElementById("roadTemplate").cloneNode(true);
+    templateBase["sidewalk"] = document.getElementById("sidewalkTemplate").cloneNode(true);
+    templateBase["bollard"] = document.getElementById("bollardTemplate").cloneNode(true);
+
+    //setting template
+    templateBase["road"].removeAttribute("id");
+    templateBase["sidewalk"].removeAttribute("id");
+    templateBase["bollard"].removeAttribute("id");
+
+    //setting road component template
+    roadComponentTemplate = document.getElementById("roadComponentTemplate").cloneNode(true);
+    roadComponentTemplate.removeAttribute("id");
+
+    //initalize land
+    LandInit();
+}
+
+function AddHitbox(referencePos = null) {
+    const hitboxTemplate = document.getElementById("hitboxTemplate");
+    const emptyRoadComponentTemplate = document.getElementById("emptyRoadComponentTemplate");
+
+    var hitbox = hitboxTemplate.cloneNode(true);
+    var emptyComp = emptyRoadComponentTemplate.cloneNode(true);
+
+    hitbox.id = "hb" + hitboxCounter.toString();
+    emptyComp.id = hitbox.id + "c";
+
+
+    if (referencePos === null) {
+
+        hitbox.style.position = "absolute";
+        landElement.appendChild(emptyComp);
+        landElement.appendChild(hitbox);
+    } else {
+        referencePos.appendChild(hitbox);
+        landElement.insertBefore(emptyComp, referencePos.nextSibling);
+        //landElement.insertBefore(hitbox, emptyComp);
+    }
+
+    ++hitboxCounter;
+}
+
+function RemoveHitbox(hitboxId) {
+    document.getElementById(hitboxId).remove();
+    document.getElementById(hitboxId + "c").remove();
+}
+
+function InsertComponent(hitboxId, move = false) {
+    const hitbox = document.getElementById(hitboxId);
+    const emptyComp = document.getElementById(hitboxId + "c");
+    const componentType = dragElement.getAttribute("component");
+    var component = roadComponentTemplate.cloneNode(true);
+    var nextComp = null;
+
+    component.id = "comp" + componentCounter.toString();
+    ++componentCounter;
+
+    component.style.width = emptyComp.style.width;
+    component.setAttribute("component", componentType);
+    component.setAttribute("originHitbox", hitboxId);
+
+    nextComp = emptyComp.nextSibling;
+    if (!nextComp.classList.contains("roadComponent")) {
+        nextComp = null;
+    }
+
+    if (!move) {
+        component.appendChild(templateBase[componentType].cloneNode(true));
+    } else {
+        var target = document.getElementById(dragElement.getAttribute("target"));
+        //console.log({ target });
+
+        RemoveHitbox(target.children[1].id);
+        component.append(...target.childNodes);
+        RemoveComponent(target);
+    }
+
+    landElement.insertBefore(component, emptyComp.nextSibling);
+    AddHitbox(component);
+
+    if (nextComp !== null) {
+        nextComp.setAttribute("originHitbox", component.children[1].id);
+    }
+}
+
+function RemoveComponent(target) {
+    //console.log("remove component");
+    //console.log({target});
+    if (target.lastChild !== null) {
+        if (target.lastChild.classList.contains("hitbox")) {
+            RemoveHitbox(target.lastChild.id);
+        }
+    }
+    target.remove();
+}
+
+function EnterHitbox(event) {
+    if (landElement.hasAttribute("hitOn")) {
+        //console.log("enter hitbox");
+        var refPercent = 100.0 / landWidth;
+        var emptyComp = document.getElementById(event.srcElement.id + "c");
+        if (dragElement.classList.contains("roadComponent")) {
+            document.getElementById(dragElement.getAttribute("target")).style.opacity = "0.3";
+        }
+
+        emptyComp.style.width = parseFloat(refPercent * parseFloat(componentDefaultWidth[dragElement.getAttribute("component")])).toString() + "%";
+        inHitboxId = event.target.id;
+    }
+}
+
+function LeaveHitbox(event) {
+    if (landElement.hasAttribute("hitOn")) {
+        inHitboxId = null;
+        //console.log("leave hitbox");
+
+        if (dragElement.classList.contains("roadComponent")) {
+            document.getElementById(dragElement.getAttribute("target")).style.opacity = "1";
+        }
+
+        var emptyComp = document.getElementById(event.srcElement.id + "c");
+        emptyComp.style.width = "0px";
+    }
+}
+
+function EnterTrashcan() {
+    //console.log("enter trash can");
+    if (dragElement === null) return;
+    if (dragElement.classList.contains("roadComponent")) {
+        document.getElementById(dragElement.getAttribute("target")).style.opacity = "0.3";
+    }
+    inHitboxId = -1;
+}
+
+function LeaveTrashcan() {
+    //console.log("leave trash can");
+    if (dragElement === null) return;
+
+    if (dragElement.classList.contains("roadComponent")) {
+        document.getElementById(dragElement.getAttribute("target")).style.opacity = "1";
+    }
+    inHitboxId = null;
+}
+
+function ComponentDragStart(event) {
+
+    //set touch event flag
+    var touchevent = false;
+    if (event.type == "touchstart") {
+        touchevent = true;
+        //console.log("touch start")
+    }
+
+    draging = false;
+    var target = event.srcElement;
+    const xOffset = -target.clientWidth / 2;
+    const yOffset = -target.clientHeight / 2;
+
+    if (dragElement !== null) {
+        return
+    }
+
+    //if draging placed component
+    if (target.classList.contains("component")) {
+        target = target.parentElement;
+    }
+
+    //drag element setting
+    dragElement = target.cloneNode(true);
+    dragElement.setAttribute("target", dragElement.id);
+    dragElement.removeAttribute("id");
+    dragElement.removeAttribute("onmousedown");
+    dragElement.style.removeProperty("opacity");
+    dragElement.style.width = target.clientWidth + "px";
+    dragElement.style.height = target.clientHeight + "px";
+
+
+    //move drag element to the poition of the pointer/finger
+    /*
+    if (touchevent) {
+        dragElement.style.left = event.touches[0].clientX + xOffset + "px";
+        dragElement.style.top = event.touches[0].clientY + yOffset + "px";
+    } else {
+        dragElement.style.left = event.clientX + xOffset + "px";
+        dragElement.style.top = event.clientY + yOffset + "px";
+    }
+    //*/
+    dragElement.classList.add("draggingElement");
+    dragElement.style.position = "absolute";
+
+    //attach the draging element
+    document.body.appendChild(dragElement);
+
+    //remove all hitbox in the drag element
+    const hitbox = dragElement.querySelectorAll('.hitbox');
+    hitbox.forEach(box => {
+        box.remove();
+    });
+
+    //adding  event listener
+    if (touchevent) {
+        document.body.addEventListener("touchmove", ComponentDrag);
+        document.body.addEventListener("touchend", ComponentDragEnd);
+    } else {
+        document.body.addEventListener("mousemove", ComponentDrag);
+        document.body.addEventListener("mouseup", ComponentDragEnd);
+    }
+
+}
+
+function ComponentDrag(event) {
+    var touchEvent = false;
+    if (event.type === "touchmove") {
+        touchEvent = true;
+    }
+
+    if (!draging) {
+        //set the hit on flag
+        landElement.classList.add("hitOn");
+        landElement.setAttribute("hitOn", "");
+        if (dragElement.classList.contains("roadComponent")) {
+            editorElement.classList.add("hitOn");
+            editorElement.setAttribute("hitOn", "");
+        }
+        draging = true;
+    }
+    const target = dragElement;
+    const xOffset = -target.clientWidth / 2;
+    const yOffset = -target.clientHeight / 2;
+
+    if (touchEvent) {
+        target.style.left = event.touches[0].clientX + xOffset + "px";
+        target.style.top = event.touches[0].clientY + yOffset + "px";
+        const overElement = document.elementFromPoint(event.touches[0].clientX, event.touches[0].clientY);
+        if (overElement !== null) {
+            if (overElement.classList.contains("hitbox") || overElement.id === "trashcan") {
+                if (inHitboxId !== null) {
+                    if (inHitboxId === -1) {
+                        if (overElement.id !== "trashcan") {
+                            document.getElementById("trashcan").dispatchEvent(new Event("mouseleave"));
+                        }
+                    } else if (overElement.id !== inHitboxId) {
+                        document.getElementById(inHitboxId).dispatchEvent(new Event("mouseleave"));
+                    }
+                }
+
+                touchHitbox = true;
+                var event = new Event("mouseenter");
+                overElement.dispatchEvent(event);
+            } else if (inHitboxId !== null) {
+                if (inHitboxId === -1) {
+                    document.getElementById("trashcan").dispatchEvent(new Event("mouseleave"));
+                } else {
+                    document.getElementById(inHitboxId).dispatchEvent(new Event("mouseleave"));
+                }
+            }
+        } else if (touchHitbox) {
+            touchHitbox = false;
+            if (inHitboxId === -1) {
+                document.getElementById("trashcan").dispatchEvent(new Event("mouseleave"));
+            } else {
+                document.getElementById(inHitboxId).dispatchEvent(new Event("mouseleave"));
+            }
+        }
+
+    } else {
+        target.style.left = event.clientX + xOffset + "px";
+        target.style.top = event.clientY + yOffset + "px";
+    }
+
+}
+
+function ComponentDragEnd(event) {
+
+    var touchEvent = false;
+    if (event.type === "touchend") {
+        touchEvent = true;
+        touchHitbox = false;
+        //console.log("touch end");
+        document.body.removeEventListener("touchmove", ComponentDrag);
+        document.body.removeEventListener("touchend", ComponentDragEnd);
+    } else {
+        document.body.removeEventListener("mousemove", ComponentDrag);
+        document.body.removeEventListener("mouseup", ComponentDragEnd);
+
+    }
+
+    if (draging === false) {
+        console.log("click");
+    }
+
+    //console.log("drag end");
+    landElement.classList.remove("hitOn");
+    landElement.removeAttribute("hitOn");
+    editorElement.classList.remove("hitOn");
+    editorElement.removeAttribute("hitOn");
+
+    if (inHitboxId !== null) {
+        var emptyComp = document.getElementById(inHitboxId + "c");
+        if (dragElement.classList.contains("roadComponent")) {
+            var target = document.getElementById(dragElement.getAttribute("target"));
+            document.getElementById(dragElement.getAttribute("target")).style.opacity = "1";
+            if (inHitboxId === -1) {
+                RemoveComponent(target);
+                inHitboxId = null;
+                dragElement.remove();
+                dragElement = null;
+                return;
+            } else {
+                if (target.children[1].id !== inHitboxId) {
+                    InsertComponent(inHitboxId, true);
+                }
+            }
+        } else if (inHitboxId !== -1) {
+            InsertComponent(inHitboxId);
+        } else {
+            inHitboxId = null;
+            dragElement.remove();
+            dragElement = null;
+            return;
+        }
+        inHitboxId = null;
+
+        emptyComp.style.width = "0px";
+        emptyComp.style.display = "none";
+        setTimeout(() => {
+            emptyComp.style.display = "block";
+        }, 300);
+    }
+    dragElement.remove();
+    dragElement = null;
+
+}
