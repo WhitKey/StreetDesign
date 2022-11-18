0a93b93b7eff9723b18f3e78af92afd7ad404c17
TTTTTTT THOMAS <0716thomas@gmail.com>
2022-11-19 01:53:30 +0800

    [Update] add entry setting pages

--- public/entryPage/javascripts/entrySetting.js
+++ public/entryPage/javascripts/entrySetting.js
@@ -8,13 +8,110 @@
 const LandMaxWidth = 100;
 const LandMinWidth = 1;
 const SessionStorageName = "entryConfig";
+const editorLocation = "/";
+const editorLoadingTime = 1000;
+
+const Pages = {
+	title:{
+		content:`<div class="pageinner" style="/*background-color:green;*/"></div>`
+	},
+
+	page1:{
+		content:`<div class = "pageinner" ><div style="height:100%;width:80%;background-color:cyan;"></div></div>`,
+		onload: TestFunc
+	},
+
+	loadLocal:{
+		content:`
+<div class = "pageinner">
+	<div class = "questionContainer">
+		<span class="question">載入之前的存檔?</span>
+		<div class="flowArea">
+			<button class="next" onclick="OnSwitchPage(1, 'selectRoadType')">新的道路</button>
+			<button class="next" onclick="OnSwitchPage(-1, 'ToEditor')">載入存檔</button>
+		</div>
+	</div>
+</div>
+`
+	},
+
+	ToEditor:{
+		content:`
+		<div class = "pageinner" style="background-color:green">
+		</div>
+		`,
+		onload:ToEditorCallback
+	},
+
+	selectRoadType:{
+		content:`
+<div class = "pageinner" >
+	<div class="questionContainer">
+		<span class="question">請選擇道路種類</span>
+		<select id="roadTypeSelect" class="form-select form-select-lg mb-5" aria-label=".form-select-lg example" config="roadType" onchange="SaveOnChange(event);">
+			<option value="primary" >主要/次要道路</option>
+			<option value="service">服務性道路</option>
+		</select>
+		<div class="flowArea">
+			<button class="prev" onclick="OnSwitchPage(-1, 'loadLocal')">上一步</button>
+			<button class="next" onclick="OnSwitchoutRoadType('selectRoadSide')">下一步</button>
+		</div>
+	</div>
+</div>
+		`,
+		onload:OnLoadSelectRoadType,
+	},
+
+	selectRoadSide:{
+		content:`
+<div class = "pageinner" >
+	<div class="questionContainer">
+		<span class="question">請選擇鄰接土地建物型式</span>
+		<select id="roadSideSelect" class="form-select form-select-lg mb-5" aria-label=".form-select-lg example" config="hasArcade" onchange="SaveOnChange(event);">
+			<option value="false" >無 騎樓 / 退縮空間</option>
+			<option value="true">有 騎樓 / 退縮空間</option>
+		</select>
+		<div class="flowArea">
+			<button class="prev" onclick="OnSwitchPage(-1, 'selectRoadType')">上一步</button>
+			<button class="next" onclick="OnSwitchoutRoadSide('inputRoadWidth')">下一步</button>
+		</div>
+	</div>
+</div>
+		`,
+		onload:()=>{RestoreValue("roadSideSelect")},
+	},
+
+	inputRoadWidth:{
+		content:`
+<div class = "pageinner" >
+	<div class="questionContainer">
+		<span class="question">請選擇入腹地寬度</span>
+		<div class="input-group mb-3 mt-4" style="max-height:40px; overflow:hidden;">
+			<input id="roadWidthInput" onchange="OnChangeLandWidth(event);" type="number" class="form-control" value="15" min="1" max="100" step="0.1" aria-describedby="basic-addon1" onchange="OnChangeLandWidth(event);">
+			<span class="input-group-text" id="basic-addon1">m</span>
+		</div>
+		<div class="flowArea">
+			<button class="prev" onclick="OnSwitchPage(-1, 'selectRoadSide')">上一步</button>
+			<button class="next" onclick="OnSwitchoutRoadWidth('page1')">下一步</button>
+		</div>
+	</div>
+</div>
+		`,
+		onload:()=>{RestoreValue("roadSideSelect")},
+	}
+}
+
+
+let hasTempStorage = false;
 
 let PageElement = document.getElementById("page");
 
 let entryConfig = {
-	loadLocal: false,
 	loadExtern: false,
-	landWidth: 15
+	landWidth: 15,
+	roadType: "primary",
+	hasArcade: "false",
+	template: []
 };
 
 
@@ -25,9 +122,21 @@ let entryConfig = {
 //
 //--------------------------
 window.OnLoad = function(){
+	let tempStorage = localStorage.getItem("tempStorage");
+
 	console.log("load");
 	PageElement = document.getElementById("page");
-	//LoadPage(-1, "page0");
+
+	//setup default page
+	PageElement.innerHTML = `<div id="currPage" class="pageFrame currPage">${Pages.title.content}</div>`;
+
+	if(tempStorage){
+		setTimeout(()=>{LoadPage(1, "loadLocal");}, 500);
+		hasTempStorage = true;
+	}else{
+		setTimeout(()=>{LoadPage(1, "selectRoadType");}, 500);
+
+	}
 }
 
 //--------------------------
@@ -35,35 +144,19 @@ window.OnLoad = function(){
 // Page control function
 //
 //--------------------------
-function GetPage(key){
-	if(key === "page0"){
-		return `<div class = "pageinner" style="display:flex;justify-content:center" style="background-color:lightgray;"></div>`
-	}
-	if(key === "page1"){
-		return `<div class = "pageinner" style="display:flex;justify-content:center"><div style="height:100%;width:80%;background-color:cyan;"></div></div>`
-	}
-	if(key === "page2"){
-		return `<div class = "pageinner" style="display:flex;justify-content:center"><div style="height:100%;width:80%;background-color:blue;"></div></div>`
-	}
-	if(key === "page3"){
-		return `<div class = "pageinner" style="display:flex;justify-content:center"><div style="height:100%;width:80%;background-color:red;"></div></div>`
-	}
-	return ``;
-}
-
 function LoadPage(direction, pageKey){
-
-	console.log("load page");
-
 	//create new page
 	let newPage = document.createElement("div");
 	let oldPage = document.getElementById("currPage");
-	
-	oldPage.id = "";
+	let newPageInfo = Pages[pageKey];
+
+	if(newPageInfo === undefined)return;
+
+	oldPage.removeAttribute("id");
 	
 	newPage.classList.add("pageFrame");
 	newPage.id = "currPage";
-	newPage.innerHTML = GetPage(pageKey);
+	newPage.innerHTML = newPageInfo.content;
 
 	//inject element
 	if(direction === 1){
@@ -72,18 +165,28 @@ function LoadPage(direction, pageKey){
 		oldPage.before(newPage);
 	}
 	
+	if(newPageInfo.onload){
+		setTimeout((target, callback)=>{callback(target);}, 10, newPage, newPageInfo.onload);
+	}
+
 	setTimeout((oldPage, newPage) => {
 		newPage.classList.add("currPage");
 		oldPage.classList.remove("currPage");
-		setTimeout((oldPage)=>{oldPage.remove();}, 600, oldPage);
+		setTimeout((oldPage)=>{oldPage.remove();}, 400, oldPage);
 	}, 10, oldPage, newPage);
+}
+
+function ToEditorCallback(page){
+	setTimeout(()=>{window.location.replace(editorLocation);}, editorLoadingTime);
 }
 
 window.OnSwitchPage = function(direction, pageKey){
 	LoadPage(direction, pageKey);
 }
 
-
+function TestFunc(page){
+	console.log(entryConfig);
+}
 //--------------------------
 //
 // storage functions
@@ -91,6 +194,20 @@ window.OnSwitchPage = function(direction, pageKey){
 //--------------------------
 function SaveToSession(){
 	sessionStorage.setItem(SessionStorageName, JSON.stringify(entryConfig));
+}
+
+function RestoreValue(id){
+	let target = document.getElementById(id);
+	let prop = target.getAttribute("config");
+
+	target.value = entryConfig[prop];
+}
+
+window.SaveOnChange = function(event){
+	let target = event.target;
+	let prop = target.getAttribute("config");
+
+	entryConfig[prop] = target.value;
 }
 
 
@@ -112,7 +229,45 @@ window.OnChangeLandWidth = function(event){
 	entryConfig.landWidth = parseFloat(value);
 }
 
-window.ToEditor = function(){
-	SaveToSession();
-	window.location.replace('/');
+window.OnSwitchoutRoadWidth = function(nextPage){
+	console.log( document.getElementById("roadWidthInput"));
+	let value = document.getElementById("roadWidthInput").value;
+	if(value > LandMaxWidth){
+		value = LandMaxWidth;
+	}
+	else if(value < LandMinWidth){
+		value = LandMinWidth;
+	}
+	entryConfig.landWidth = parseFloat(value);
+
+	LoadPage(1, nextPage);
 }
+
+
+//--------------------------
+//
+// road type functions
+//
+//--------------------------
+function OnLoadSelectRoadType(page){
+	RestoreValue("roadTypeSelect");
+	if(hasTempStorage)return;
+	page.getElementsByClassName("prev")[0].remove();
+}
+
+window.OnSwitchoutRoadType = function(nextPage){
+	entryConfig.roadType = document.getElementById("roadTypeSelect").value;
+	LoadPage(1, nextPage);
+}
+
+//--------------------------
+//
+// road side functions
+//
+//--------------------------
+window.OnSwitchoutRoadSide = function(nextPage){
+	entryConfig.hasArcade = document.getElementById("roadSideSelect").value;
+	LoadPage(1, nextPage);
+}
+
+
